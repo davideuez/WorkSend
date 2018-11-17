312e00c320d42a51629262f93d8b5adfe47d8252
davideUez <davideuez98@hotmail.it>
2018-11-17 12:52:54 +0100

    Fixing error in test file

--- api/tests/get_classes/get_classes.test.js
+++ api/tests/get_classes/get_classes.test.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 const url = "https://worksend-develop.herokuapp.com/classes"
 
-it('works with get', async () => {
+test('works with get', async () => {
   expect.assertions(1)
   var response = await fetch(url)
   expect(response.status).toEqual(200)
