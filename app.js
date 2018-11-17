0eddc19dbf04efb19bfdf1c1c62ad36c5454ef9f
davideuez <davide.uez@studenti.unitn.it>
2018-11-17 15:55:44 +0100

    Trying to fix privacy settings

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv");
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+dotenv.config({
+  path: './.env'
+});
+
 const classesRoute = require('./api/routes/classes');
 
-const db = mongoose.connect('mongodb://davide_uez:WorkSendPW@worksend-shard-00-00-l1h5a.mongodb.net:27017,worksend-shard-00-01-l1h5a.mongodb.net:27017,worksend-shard-00-02-l1h5a.mongodb.net:27017/Worksend_DB?ssl=true&replicaSet=WorkSend-shard-0&authSource=admin&retryWrites=true');
+const db = mongoose.connect('mongodb://davide_uez:' + process.env.DB_PASS + '@worksend-shard-00-00-l1h5a.mongodb.net:27017,worksend-shard-00-01-l1h5a.mongodb.net:27017,worksend-shard-00-02-l1h5a.mongodb.net:27017/Worksend_DB?ssl=true&replicaSet=WorkSend-shard-0&authSource=admin&retryWrites=true');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
