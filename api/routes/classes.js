1461973396157850d6899042323d2120f46131eb
davideUez <davideuez98@hotmail.it>
2018-11-17 10:34:12 +0100

    Create classes route and connect to a MongoDB database

---
+++ api/routes/classes.js
@@ -0,0 +1,164 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Class = require('../models/class');
+
+// Ritorna l'intera lista di classi presenti
+
+router.get('/', (req, res, next) => {
+  Class.find()
+    .select('name description keyword _id')
+    .exec()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        classes: docs.map(doc => {
+          return {
+            name: doc.name,
+            description: doc.description,
+            keyword: doc.keyword,
+            _id: doc._id,
+            request: {
+              type: 'GET',
+              description: 'GET_SINGLE_CLASS_BY_ID',
+              url: 'http://localhost:3000/classes/' + doc._id
+            }
+          }
+        })
+      }
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+// Crea una classe
+
+router.post('/', (req, res, next) => {
+
+  const classInfo = new Class({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    description: req.body.description,
+    keyword: req.body.keyword
+  });
+
+  classInfo.save()
+    .then(result => {
+      res.status(201).json({
+        message: "Handling POST requests to /classes",
+        createdClass: {
+          name: result.name,
+          description: result.description,
+          keyword: result.keyword,
+          _id: result._id,
+          request: {
+            type: 'GET',
+            description: 'GET_CLASS_INFO',
+            url: 'http://localhost:3000/classes/' + result._id
+          }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+
+});
+
+// Ritorna la classe con l'id passato come parametro
+
+router.get('/:classId', (req, res, next) => {
+  const id = req.params.classId;
+  Class.findById(id)
+    .select('name description keyword _id')
+    .exec()
+    .then(doc => {
+      if (doc) {
+        res.status(200).json({
+          class: doc,
+          request: {
+            type: 'GET',
+            description: 'GET_ALL_CLASSES',
+            url: 'http://localhost:3000/classes'
+          }
+        });
+      } else
+        res.status(404).json({
+          message: 'Non ho trovato una classe con questo id'
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+
+});
+
+
+// Modifica la classe tramite l'id passato come parametro
+
+router.patch('/:classId', (req, res, next) => {
+  const id = req.params.classId
+  const updateOps = {}
+
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+
+  Class.update({
+      _id: id
+    }, {
+      $set: updateOps
+    })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'Classe aggiornata con successo',
+        request: {
+          type: 'GET',
+          description: 'GET_MODIFIED_CLASS',
+          url: 'http://localhost:3000/classes/' + id
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+// Elimina la classe in base all' id passato come parametro
+
+router.delete('/:classId', (req, res, next) => {
+  const id = req.params.classId
+  Class.remove({
+      _id: id
+    })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'Classe cancellata con successo'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const classesRoute = require('./api/routes/classes');
+
+const db = mongoose.connect('mongodb://davide_uez:WorkSendPW@worksend-shard-00-00-l1h5a.mongodb.net:27017,worksend-shard-00-01-l1h5a.mongodb.net:27017,worksend-shard-00-02-l1h5a.mongodb.net:27017/Worksend_DB?ssl=true&replicaSet=WorkSend-shard-0&authSource=admin&retryWrites=true');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
 
-app.use('/', express.static('public/login'));
+// Percorsi per gestire le richieste 
 
-app.listen(port, function() {
-    console.log('Server running on port ', port);
-    });
+app.get('/', function (req, res) {
+  res.send('You are in the main route!');
+});
 
-// Handling GET requests
-app.get('/', function(req, res){ 
-    res.send('You are in the main route!'); 
-  });
+app.use('/classes', classesRoute);
 
-app.get('/hello', function(req, res){ 
-    res.send('Hello World!'); 
-  });
+
+// Server in ascolto sulla porta ...
+app.listen(port, function () {
+  console.log('Server running on port ', port);
+});
