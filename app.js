97d8335779004f7320c98b8a065a973a4638c064
davideuez <davide.uez@studenti.unitn.it>
2018-11-17 18:35:20 +0100

    Fixing all issues

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config({
 
 const classesRoute = require('./api/routes/classes');
 
-const db = mongoose.connect('mongodb://davide_uez:' + process.env.DB_PASS + '@worksend-shard-00-00-l1h5a.mongodb.net:27017,worksend-shard-00-01-l1h5a.mongodb.net:27017,worksend-shard-00-02-l1h5a.mongodb.net:27017/Worksend_DB?ssl=true&replicaSet=WorkSend-shard-0&authSource=admin&retryWrites=true');
+const db = mongoose.connect('mongodb://davide_uez:WorkSendPW@worksend-shard-00-00-l1h5a.mongodb.net:27017,worksend-shard-00-01-l1h5a.mongodb.net:27017,worksend-shard-00-02-l1h5a.mongodb.net:27017/Worksend_DB?ssl=true&replicaSet=WorkSend-shard-0&authSource=admin&retryWrites=true');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
