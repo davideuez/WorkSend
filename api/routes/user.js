36c0a224722df45ee8b6d49fe800a326dc5a05af
davideuez <davide.uez@studenti.unitn.it>
2018-11-20 23:10:34 +0100

    Added the DELETE route for Users and add another check to be sure that the e-mail is unique

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,124 +5,74 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
-// Ritorna l'intera lista di utenti
-
-router.get('/', (req, res, next) => {
-  User.find()
-    .select('_id email')
-    .exec()
-    .then(docs => {
-      const response = {
-        count: docs.length,
-        users: docs.map(doc => {
-          return {
-            _id: doc._id,
-            email: doc.email,
-            request: {
-              type: 'GET',
-              description: 'GET_ALL_USERS',
-              url: 'https://worksend.herokuapp.com/users/'
-            }
-          }
-        })
-      }
-      res.status(200).json(response);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-});
-
-// Crea una classe
+// Crea un'utente
 
 router.post('/signup', (req, res, next) => {
 
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({
-        error: err
-      });
-    } else {
-
-      const userInfo = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: hash
-      });
-
-      userInfo.save()
-        .then(result => {
-          res.status(201).json({
-            message: "Succesfull POST requests to /users",
-            createdClass: {
-              _id: result.id,
-              email: result.email,
-              password: result.password,
-              request: {
-                type: 'GET',
-                description: 'GET_USER_INFO',
-                url: 'https://worksend.herokuapp.com/users/' + result._id
-              }
-            }
-          });
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json({
-            error: err
-          });
+  User.find({
+      email: req.body.email
+    }).exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "E-mail already exists!"
         });
+      } else {
 
-    }
-  });
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              err: err
+            });
+          } else {
 
-});
+            const userInfo = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash
+            });
 
-// Ritorna la classe con l'id passato come parametro
+            userInfo.save()
+              .then(result => {
+                res.status(201).json({
+                  message: "Succesfull POST requests to /users",
+                  createdClass: {
+                    _id: result.id,
+                    email: result.email,
+                    password: result.password,
+                    request: {
+                      type: 'GET',
+                      description: 'GET_USER_INFO',
+                      url: 'https://worksend.herokuapp.com/users/' + result._id
+                    }
+                  }
+                });
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                  error: err
+                });
+              });
 
-router.get('/:classId', (req, res, next) => {
-  const id = req.params.classId;
-  Class.findById(id)
-    .select('name description keyword _id')
-    .exec()
-    .then(doc => {
-      if (doc) {
-        res.status(200).json({
-          class: doc,
-          request: {
-            type: 'GET',
-            description: 'GET_ALL_CLASSES',
-            url: 'https://worksend.herokuapp.com/classes'
           }
         });
-      } else
-        res.status(404).json({
-          message: 'Non ho trovato una classe con questo id'
-        });
+      }
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
 
 });
 
-// Elimina la classe in base all' id passato come parametro
+// Elimina un'utente dal database
 
-router.delete('/:classId', (req, res, next) => {
-  const id = req.params.classId
-  Class.remove({
-      _id: id
+router.delete('/:userId', (req, res, next) => {
+
+  User.remove({
+      _id: req.params.userId
     })
     .exec()
     .then(result => {
       res.status(200).json({
-        message: 'Classe cancellata con successo'
+        message: 'User successfully deleted'
       });
     })
     .catch(err => {
