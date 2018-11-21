9d1424755f747abadfef4cebaa57dfe9777a7c4f
davideuez <davide.uez@studenti.unitn.it>
2018-11-21 11:09:45 +0100

    Bug fixed

--- api/routes/user.js
+++ api/routes/user.js
@@ -87,7 +87,7 @@ router.post('/login', (req, res, next) => {
               email: users[0].email,
               userId: users[0]._id
             },
-            "" + process.env.JWT_KEY, {
+            "secretWRKSNDPW", {
               expiresIn: "1h"
             })
 
